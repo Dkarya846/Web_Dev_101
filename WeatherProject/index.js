51ff3004e7c7e0935a123d1d4e42c3a9f33e24bb
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2021-10-07 06:43:13 +0530

    added Weather Project

--- WeatherProject/index.js
+++ WeatherProject/index.js
@@ -1,18 +1,41 @@
 const { response } = require("express");
 const express = require("express");
-
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Server is up and running...");
-  https.get(
-    "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=47657b66139fc2cb733e7566ec239fd1&units=metric",
-    (response) => {
-      console.log(response);
-    }
-  );
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.city;
+  const unit = "metric";
+  const apiKey = process.env.API_KEY;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&units=" +
+    unit +
+    "&appid=" +
+    apiKey;
+  https.get(url, (response) => {
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const desc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(`<p>The weather is currently ${desc}</p>`);
+      res.write(
+        `<h1>The Temperature in ${query} is ${temp} degree celcius</h1>`
+      );
+      res.write("<img src=" + iconUrl + " />");
+      res.send();
+    });
+  });
 });
 
 app.listen(3000, () => {
