dca3020029a140b974c49f883b1ffdcba8d13aea
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2021-11-02 12:20:36 +0530

    added todo list

--- todo-list-v1/app.js
+++ todo-list-v1/app.js
@@ -5,28 +5,42 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", (req, res) => {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { day, items });
+  res.render("list", { listTitle: day, items });
 });
 
 app.post("/", (req, res) => {
-  var item = req.body.new_item;
-  items.push(item);
-  console.log(items);
-  res.redirect("/");
+  let item = req.body.new_item;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", items: workItems });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen("3000", () => {
