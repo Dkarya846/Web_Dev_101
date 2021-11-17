cf187415e390e1d2b9d77bdc55107165c6715ce3
Deepak Kumar <dkarya.delhi.uK@gmail.com>
2021-11-17 15:04:52 +0530

    completed the API methods for wiki API

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -14,21 +14,106 @@ const articlesSchema = mongoose.Schema({
   title: {
     type: String,
     required: true
+  }, content: {
+    type: String,
+    required: true
   }
 })
 
 const Article = mongoose.model("Article", articlesSchema);
 
 
-app.get("/articles", (req, res) => {
-  Article.find((err, articles) => {
-    if (!err) {
-      return res.send(articles);
-    } else {
-      res.send(err);
-    }
-  });
-})
+//Handles Get request for articles routes
+
+app.route("/articles")
+  .get((req, res) => {
+    Article.find((err, articles) => {
+      if (!err) {
+        return res.send(articles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post((req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const article = new Article({
+      title, content
+    });
+
+    article.save((err) => {
+      if (!err) {
+        res.send("Successfully saved the article.", 200)
+      }
+      else {
+        res.send(err);
+      }
+    })
+
+  })
+
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err) {
+        res.send("Successfully deleted all articles", 200);
+      }
+      else {
+        res.send(err);
+      }
+    })
+  })
+
+
+app.route("/articles/:articleTitle")
+  .get((req, res) => {
+    Article.findOne({ title: req.params.articleTitle }, (err, article) => {
+      console.log(article, err);
+      if (article) {
+        res.send(article);
+      }
+      else {
+        res.send("Article Not Found", 404);
+      }
+    })
+  })
+
+  .put((req, res) => {
+    console.log("/get", req.params.articleTitle, req.body.title, req.body.content)
+    Article.replaceOne({ title: req.params.articleTitle }, { title: req.body.title, content: req.body.content }, (err) => {
+      if (!err) {
+        res.send("Article Updated Successfully");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .patch((req, res) => {
+    Article.updateOne({ title: req.params.articleTitle }, { $set: req.body }, (err) => {
+      if (!err)
+        res.send("Article Updated Successfully");
+      else {
+        res.send(err);
+      }
+    })
+  })
+
+  .delete((req, res) => {
+    Article.deleteOne({ title: req.params.articleTitle }, (err, deleted) => {
+      if (!err && deleted.deletedCount) {
+        res.send("Article Deleted Successfully.");
+      }
+      else if (!deleted.deletedCount)
+        res.send("Article Does Not Found", 404);
+      else {
+        res.send(err);
+      }
+    })
+  })
+
 
 app.listen("3000", () => {
   console.log("Server started successfully");
